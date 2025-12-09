b76c5655eeeb897962a80160b9af673b446b9f59
Jamily Grazielle <jamilygrazielle@acad.ifma.edu.br>
2025-12-09 16:39:40 -0300

    tirando comentários excessivos

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'silab';
-$username = 'root'; // altere conforme necessário
-$password = '';     // altere conforme necessário
+$username = 'root';
+$password = '';    
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
